5a88535952115bea5bad1f31e678975540c118fa
Yossi Nivin <ynivin@gmail.com>
2013-11-15 09:35:25 -0500

    adding 'b' and ,git ignore

--- letter_tasks.v
+++ letter_tasks.v
@@ -1,196 +1,61 @@
 module letter_tasks();
 	
-		reg[9:0] lines;
-	task print_a;
+reg[9:0] lines;
+
+	task print_space;
 	begin
-		fork
-			begin
-				lines[9] = 0;
-				#1;
-				lines[9] = 1;
-				#1;
-				lines[9] = 0;
-				#1;
-				lines[9] = 1;
-				#1;
-				lines[9] = 0;
-				#1;
-				lines[9] = 1;
-				#1;
-				lines[9] = 0;
-				#1;
-				lines[9] = 1;
-				#1;
-				lines[9] = 0;
-				#2;
-			end
-			begin
-				lines[8] = 1;
-				#1;
-				lines[8] = 0;
-				#1;
-				lines[8] = 1;
-				#1;
-				lines[8] = 0;
-				#3;
-				lines[8] = 1;
-				#1;
-				lines[8] = 0;
-				#1;
-				lines[8] = 1;
-				#1;
-				lines[8] = 0;
-				#1;
-			end
-			begin
-				lines[7] = 1;
-				#1;
-				lines[7] = 0;
-				#1;
-				lines[7] = 1;
-				#1;
-				lines[7] = 0;
-				#3;
-				lines[7] = 1;
-				#1;
-				lines[7] = 0;
-				#1;
-				lines[7] = 1;
-				#1;
-				lines[7] = 0;
-				#1;
-			end
-			begin
-				lines[6] = 1;
-				#1;
-				lines[6] = 0;
-				#1;
-				lines[6] = 1;
-				#1;
-				lines[6] = 0;
-				#3;
-				lines[6] = 1;
-				#1;
-				lines[6] = 0;
-				#1;
-				lines[6] = 1;
-				#1;
-				lines[6] = 0;
-				#1;
-			end
-			begin
-				lines[5] = 1;
-				#1;
-				lines[5] = 0;
-				#1;
-				lines[5] = 1;
-				#1;
-				lines[5] = 0;
-				#3;
-				lines[5] = 1;
-				#1;
-				lines[5] = 0;
-				#1;
-				lines[5] = 1;
-				#1;
-				lines[5] = 0;
-				#1;
-			end
-			begin
-				lines[4] = 1;
-				#1;
-				lines[4] = 0;
-				#1;
-				lines[4] = 1;
-				#1;
-				lines[4] = 0;
-				#1;
-				lines[4] = 1;
-				#1;
-				lines[4] = 0;
-				#1;
-				lines[4] = 1;
-				#1;
-				lines[4] = 0;
-				#1;
-				lines[4] = 1;
-				#1;
-				lines[4] = 0;
-				#1;
-			end
-			begin
-				lines[3] = 1;
-				#1;
-				lines[3] = 0;
-				#1;
-				lines[3] = 1;
-				#1;
-				lines[3] = 0;
-				#3;
-				lines[3] = 1;
-				#1;
-				lines[3] = 0;
-				#1;
-				lines[3] = 1;
-				#1;
-				lines[3] = 0;
-				#1;
-			end
-			begin
-				lines[2] = 1;
-				#1;
-				lines[2] = 0;
-				#1;
-				lines[2] = 1;
-				#1;
-				lines[2] = 0;
-				#3;
-				lines[2] = 1;
-				#1;
-				lines[2] = 0;
-				#1;
-				lines[2] = 1;
-				#1;
-				lines[2] = 0;
-				#1;
-			end
-			begin
-				lines[1] = 1;
-				#1;
-				lines[1] = 0;
-				#1;
-				lines[1] = 1;
-				#1;
-				lines[1] = 0;
-				#3;
-				lines[1] = 1;
-				#1;
-				lines[1] = 0;
-				#1;
-				lines[1] = 1;
-				#1;
-				lines[1] = 0;
-				#1;
-			end
-			begin
-				lines[0] = 1;
-				#1;
-				lines[0] = 0;
-				#1;
-				lines[0] = 1;
-				#1;
-				lines[0] = 0;
-				#3;
-				lines[0] = 1;
-				#1;
-				lines[0] = 0;
-				#1;
-				lines[0] = 1;
-				#1;
-				lines[0] = 0;
-				#1;
-			end
-		join
+		lines = 10'h000;
+		#6;
+	end
+	endtask
+
+	task print_A;
+	begin
+		lines = 10'h1FF;
+		#1;
+		lines = 10'h200;
+		#1;
+		lines = 10'h1FF;
+		#1;
+		lines = 10'h200;
+		#1;
+		lines = 10'h010;
+		#1;
+		lines = 10'h200;
+		#1;
+		lines = 10'h1FF;
+		#1;
+		lines = 10'h200;
+		#1;
+		lines = 10'h1FF;
+		#1;
+		lines = 10'h000;
+		#3;
+	end
+	endtask
+
+	task print_b;
+	begin
+		lines = 10'h3FF;
+		#1;
+		lines = 10'h000;
+		#1;
+		lines = 10'h3FF;
+		#1;
+		lines = 10'h000;
+		#1;
+		lines = 10'h081;
+		#1;
+		lines = 10'h000;
+		#1;
+		lines = 10'h07E;
+		#1;
+		lines = 10'h000;
+		#1;
+		lines = 10'h07E;
+		#1;
+		lines = 10'h000;
+		#3;
 	end
 	endtask
 
@@ -199,6 +64,7 @@ module letter_tasks();
 		$dumpfile("test.vcd");
 	    $dumpvars();
 		print_a;
+		print_b;
 		$finish;
 	end
 
